0210bbffb3fd3c784938ce248a79336df9ec00f5
cristianorregodev <cristian.duquew@gmail.com>
2023-05-24 18:39:17 -0500

    Feature (Stack): Agrega listado de tecnologias

--- src/containers/Home/Hero.jsx
+++ src/containers/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Stack } from '../../helpers/prueba'
 
 export default function Hero() {
     return (
@@ -12,7 +13,20 @@ export default function Hero() {
                     Bienvenido a mi sitio web, te invito a que conozcas un poco sobre mí, veas mis
                     proyectos o leas mi blog.
                 </p>
-
+                <div className="Stack">
+                    <div className="Stack-slide-track">
+                        {Stack.map((item) => (
+                            <div key={item.id} className="Stack-slide text">
+                                {item.element}
+                            </div>
+                        ))}
+                        {Stack.map((item) => (
+                            <div key={item.id} className="Stack-slide text">
+                                {item.element}
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 <div className="Hero-buttons">
                     <Link href={'/projects'} className="btn btn-primary">
                         Proyectos
