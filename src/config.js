8b6c7b6ea8bf364de9622772cf3628a719c28991
cristianorregodev <cristian.duquew@gmail.com>
2023-05-29 22:52:20 -0500

    Fix (config): Modifica ruta API producciÃon

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export const API_URL = 'http://cristiandeveloper.test/api/v1'
-export const API_URL = 'https://cristiandeveloper.com/api/v1'
+export const API_URL = 'https://api.cristiandeveloper.com/api/v1'
